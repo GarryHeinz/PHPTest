ce448fccbedd9b80bc0f65a9f19b24052c387eb6
GarryHeinz <47884156+GarryHeinz@users.noreply.github.com>
2022-09-27 21:06:01 +0200

    test

--- index.php
+++ index.php
@@ -1,28 +1,3 @@
 <?php
-   
-   class MyFirstClass{
-
-        var $isOpen = false;
-
-        function start($str){
-            if(!$this->isOpen){   
-                echo "<html>";
-                $this->isOpen = true;
-            }
-            echo "$str <br>";
-        }
-
-        function end(){
-            if($this->isOpen){
-                echo "</html>";
-                $this->isOpen = false   ;
-            }
-        }
-   }
-
-   $class = new MyFirstClass();
-   $class->start("hallo");
-   $class->start("Hallo 1");
-   $class->start("Moingorno");
-   $class->end();
-?>  
+    require (start.php);
+?>
---
+++ start.php
@@ -0,0 +1,28 @@
+<?php
+   
+   class MyFirstClass{
+
+        var $isOpen = false;
+
+        function start($str){
+            if(!$this->isOpen){   
+                echo "<html>\n";
+                $this->isOpen = true;
+            }
+            echo "$str <br>\n";
+        }
+
+        function end(){
+            if($this->isOpen){
+                echo "</html>\n";
+                $this->isOpen = false   ;
+            }
+        }
+   }
+
+   $class = new MyFirstClass();
+   $class->start("hallo");
+   $class->start("Hallo 1");
+   $class->start("Moingorno");
+   $class->end();
+?>  
