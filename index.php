e47f6543056f420db56348ff8c01a11931e7062e
GarryHeinz <47884156+GarryHeinz@users.noreply.github.com>
2022-10-25 19:52:30 +0200

    build indizes

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
-    echo "Hallo Welt";
-    include 'start.php';
+    function createLinkList(){
+        $dir = scandir(".");
+
+        echo "<ol>";
+        foreach($dir as $item){
+            if(str_ends_with($item,".php")){
+                echo "<li>";
+                echo "$item";
+                echo "</li>";
+            }
+        }
+        echo "</ol>";
+    }
+ 
+    
 
 ?>
