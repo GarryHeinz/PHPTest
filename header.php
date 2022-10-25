104806ec8befd8dcc7d7ea99d91ed50757694ebd
GarryHeinz <47884156+GarryHeinz@users.noreply.github.com>
2022-10-25 20:13:14 +0200

    moin

---
+++ header.php
@@ -0,0 +1,18 @@
+<?php
+    function createLinkList(){
+        $dir = scandir(".");
+
+        echo "<ol>";
+        foreach($dir as $item){
+            $sub = explode(".",$item);
+            if($sub[count($sub)-1] == "php"){
+                echo "<li>";
+                echo "<a href='$item'>$sub[0]</a>";
+                echo "</li>";
+            }
+        }
+        echo "</ol>";
+    }
+    createLinkList();
+    
+?>
