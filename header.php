969d2d49b3ab12da681f70613d8fbb14e7624741
GarryHeinz <47884156+GarryHeinz@users.noreply.github.com>
2022-10-25 21:04:43 +0200

    moin

--- header.php
+++ header.php
@@ -1,29 +1,34 @@
 <?php
-    function createLinkList($path){
-        $dir = scandir($path);
-
-        echo "<ol>";
-        foreach($dir as $item){
-            $sub = explode(".",$item);
-            if(is_dir($item) && substr($item,0,1) != "."){
-                echo "<li>";
-                echo $item;
-                createLinkList($item);
-                echo "</li>";
-            }else if($sub[count($sub)-1] == "php"){
-                echo "<li>";
-                $uri = explode("/",$_SERVER["REQUEST_URI"]);
-                if($uri[count($uri)-1] == $item){
-                    echo $sub[0];
-                }else{
-                    echo "<a href='$path/$item'>$sub[0]</a>";
+    class LinkList{
+        public static $cwd = null;
+        public static function createLinkList(){
+            $dir = scandir($cwd);
+    
+            echo "<ol>";
+            foreach($dir as $item){
+                $sub = explode(".",$item);
+                if(is_dir($item) && substr($item,0,1) != "."){
+                    echo "<li>";
+                    echo $item;
+                    createLinkList($item);
+                    echo "</li>";
+                }else if($sub[count($sub)-1] == "php"){
+                    echo "<li>";
+                    $uri = explode("/",$_SERVER["REQUEST_URI"]);
+                    if($uri[count($uri)-1] == $item){
+                        echo $sub[0];
+                    }else{
+                        echo "<a href='$path/$item'>$sub[0]</a>";
+                    }
+                    echo "</li>";
                 }
-                echo "</li>";
             }
+            echo "</ol>";
         }
-        echo "</ol>";
+
     }
-    createLinkList(".");
-    echo getcwd();
-    echo "\n";
+    if(LinkList::$cwd == null){
+        LinkList::$cwd = getcwd();
+    }
+    LinkList::createLinkList();
 ?>
