48791b66459987600857484cf1276208ac55a18b
GarryHeinz <47884156+GarryHeinz@users.noreply.github.com>
2022-10-25 20:54:17 +0200

    moin

--- header.php
+++ header.php
@@ -5,19 +5,19 @@
         echo "<ol>";
         foreach($dir as $item){
             $sub = explode(".",$item);
+            echo "<li>";
             if(is_dir($item) && substr($item,0,1) != "."){
                 echo $item;
                 createLinkList($item);
             }else if($sub[count($sub)-1] == "php"){
-                echo "<li>";
                 $uri = explode("/",$_SERVER["REQUEST_URI"]);
                 if($uri[count($uri)-1] == $item){
                     echo $sub[0];
                 }else{
                     echo "<a href='$item'>$sub[0]</a>";
                 }
-                echo "</li>";
             }
+            echo "</li>";
         }
         echo "</ol>";
     }
