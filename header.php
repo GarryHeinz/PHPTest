23ed5317d2af065c29545078cb8aea3e82964473
GarryHeinz <47884156+GarryHeinz@users.noreply.github.com>
2022-10-25 20:40:45 +0200

    moin

--- header.php
+++ header.php
@@ -1,14 +1,17 @@
 <?php
-    function createLinkList(){
-        $dir = scandir(".");
+    function createLinkList($path){
+        $dir = scandir($path);
 
         echo "<ol>";
         foreach($dir as $item){
+            if(is_dir($item)){
+                createLinkList($item);
+            }
             $sub = explode(".",$item);
             if($sub[count($sub)-1] == "php"){
                 echo "<li>";
-                $path = explode("/",$_SERVER["REQUEST_URI"]);
-                if($path[count($path)-1] == $item){
+                $uri = explode("/",$_SERVER["REQUEST_URI"]);
+                if($uri[count($uri)-1] == $item){
                     echo $sub[0];
                 }else{
                     echo "<a href='$item'>$sub[0]</a>";
